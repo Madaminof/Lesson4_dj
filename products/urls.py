1ea96317a24292a0bd4318c91d2a792a8c1bcfef
Madaminof <madaminovsamandar51@gmail.com>
2024-05-03 23:33:05 +0500

    first commit

---
+++ products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import get_info, get_products, detail, add_products, update_products,delete_product
+
+
+app_name = 'products'
+urlpatterns = [
+    path('', get_info, name='get_info'),
+    path('products/<int:pk>', get_products, name='get_products'),
+    path('products/detail/<int:pk>', detail, name='detail'),
+    path('add_products', add_products, name='add_products'),
+    path('update/<int:pk>', update_products, name='update'),
+    path('delete/<int:pk>', delete_product, name='delete_product'),
+]
